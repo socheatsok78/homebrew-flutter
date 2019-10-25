f8be144db1c3d5a810505454433f14dbe1d56fb2
Socheat Sok <socheatsok78@gmail.com>
2019-10-25 14:27:33 +0700

    Update Travis CI config, Running more test on Flutter SDK

---
+++ test/flutter.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+shell_output() {
+    echo -e "=================================================="
+    echo -e "| $@"
+    echo -e "=================================================="
+}
+
+# Step 1
+shell_output "Locating Flutter binary"
+which flutter
+
+# Step 2
+shell_output "Checking installed Flutter version"
+flutter --version
+
+# Step 3
+shell_output "Available Flutter Channels"
+flutter channel
+
+shell_output "Switching to Dev Channel"
+flutter channel dev
+flutter --version
+
+shell_output "Switching to Beta Channel"
+flutter channel beta
+flutter --version
+
+shell_output "Switching to Master Channel"
+flutter channel master
+flutter --version
