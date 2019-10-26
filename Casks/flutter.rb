88d524282c0315f44216becddc411908a1d2db19
Socheat Sok <socheatsok78@gmail.com>
2019-10-26 11:57:50 +0700

    Fix Mac compressed hash for Flutter Cask

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -6,7 +6,7 @@ cask 'flutter' do
     
     if OS.mac?
         url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.9.1+hotfix.6-stable.zip"
-        sha256 "cbfa31d9bf5d4b1e5331301b607cfaf10aec7a4fa8dc4ad97b3fdc4eeba1c3f5"
+        sha256 "8d0b3e217e45fbde64e117c5932ec5bd18ced0e8e8fba80a0fec95e38854bb6a"
     elsif OS.linux?
         url "https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v1.9.1+hotfix.6-stable.tar.xz"
         sha256 "a09831ba3d90f9dcdf866e341dc547d0b60062154e5ab66d64d97bba8b6d9331"
