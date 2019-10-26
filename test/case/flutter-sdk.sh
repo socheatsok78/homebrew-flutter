d4119ef4c7e6999c902b42b72035cc91bcd198a9
Socheat Sok <socheatsok78@gmail.com>
2019-10-26 11:42:06 +0700

    Update test\case\flutter-sdk.sh, add more descriptive detail

--- test/case/flutter-sdk.sh
+++ test/case/flutter-sdk.sh
@@ -15,12 +15,12 @@ get_file_hash() {
 
     local filename="flutter_${os}_${version}-${build}.zip"
 
+    shell_output "Downloading for flutter ${os}"
     curl -L -o "$filename" "https://storage.googleapis.com/flutter_infra/releases/${build}/${os}/${filename}.zip"
+
+    shell_output "Generating hash for flutter ${os}"
     shasum -a 256 "$filename"
 }
 
-shell_output "Hashing Flutter:Latest for Mac OS"
 get_file_hash "macos" "v1.9.1+hotfix.6" "stable"
-
-shell_output "Hashing Flutter:Latest for Linux"
 get_file_hash "linux" "v1.9.1+hotfix.6" "stable"
