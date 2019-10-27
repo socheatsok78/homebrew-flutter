4d2fee158d6582450542304e5f25427f7fd11ca5
Socheat Sok <socheatsok78@gmail.com>
2019-10-27 22:50:19 +0700

    Update test\case\flutter-sdk.sh, attempt to fix file wrong file hash #2

--- test/case/flutter-sdk.sh
+++ test/case/flutter-sdk.sh
@@ -12,27 +12,35 @@ sha256() {
     shasum --algorithm 256 "$1"
 }
 
-get_file_hash() {
+hash_files() {
+    folder=$1
+    for filename in "$folder"/*; do
+        sha256 "$filename"
+    done
+}
+
+download_flutter_sdk() {
     local os="$1"
     local filename="$2"
     local build=${3:-stable}
 
-    mkdir -p "./download"
+    mkdir -p "./sdk"
 
     local url="https://storage.googleapis.com/flutter_infra/releases/${build}/${os}/${filename}"
 
     shell_output "Downloading for flutter ${os}"
     echo -e "$url\n"
+
     curl -L \
-        -o "./download/$filename" \
+        -o "./sdk/$filename" \
         $url
-
-    shell_output "Generating hash for flutter ${os}"
-    sha256 "./download/$filename"
-
-    shell_output "Listing Download Directory"
-    ls -lh
 }
 
-get_file_hash "macos" "flutter_macos_v1.9.1+hotfix.6-stable.zip"
-get_file_hash "linux" "flutter_linux_v1.9.1+hotfix.6-stable.tar.xz"
+download_flutter_sdk "macos" "flutter_macos_v1.9.1+hotfix.6-stable.zip"
+download_flutter_sdk "linux" "flutter_linux_v1.9.1+hotfix.6-stable.tar.xz"
+
+shell_output "Generating hash for flutter ${os}"
+hash_files "sdk"
+
+shell_output "Listing Download Directory"
+ls -lh "./sdk"
