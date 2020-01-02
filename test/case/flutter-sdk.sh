06b5f5814898ee572a6a0962a182e03d612f8627
Socheat Sok <alex@socheat.net>
2020-01-02 20:22:20 +0700

    Update Flutter SDK version test case for hash

--- test/case/flutter-sdk.sh
+++ test/case/flutter-sdk.sh
@@ -4,8 +4,8 @@ set -e
 
 source ./test/helper
 
-download_flutter_sdk "macos" "flutter_macos_v1.9.1+hotfix.6-stable.zip"
-download_flutter_sdk "linux" "flutter_linux_v1.9.1+hotfix.6-stable.tar.xz"
+download_flutter_sdk "macos" "flutter_macos_v1.12.13+hotfix.5-stable.zip"
+download_flutter_sdk "linux" "flutter_linux_v1.12.13+hotfix.5-stable.tar.xz"
 
 shell_output "Generating hash for flutter ${os}"
 hash_files "sdk"
