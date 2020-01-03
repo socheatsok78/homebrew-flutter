7fc9583605eb39eb5f0003d716a3d9a1c7a2ff0c
Socheat Sok <alex@socheat.net>
2020-01-03 09:32:18 +0700

    Update test generate hash for beta channel

--- test/case/flutter-sdk.sh
+++ test/case/flutter-sdk.sh
@@ -4,8 +4,13 @@ set -e
 
 source ./test/helper
 
+# Flutter Stable
 download_flutter_sdk "macos" "flutter_macos_v1.12.13+hotfix.5-stable.zip"
 download_flutter_sdk "linux" "flutter_linux_v1.12.13+hotfix.5-stable.tar.xz"
+
+# Flutter Beta
+download_flutter_sdk "macos" "flutter_macos_v1.12.13+hotfix.6-beta.zip" "beta"
+download_flutter_sdk "linux" "flutter_linux_v1.12.13+hotfix.6-beta.tar.xz" "beta"
 
 shell_output "Generating hash for flutter ${os}"
 hash_files "sdk"
